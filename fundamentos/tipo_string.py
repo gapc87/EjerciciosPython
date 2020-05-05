1b3730320ce9e0cf0d0e3bb4c3426b06ec8b269b
Gabriel Perez <gapc87@gmail.com>
2020-05-05 20:41:28 +0200

    tipos de datos string

---
+++ fundamentos/tipo_string.py
@@ -0,0 +1,10 @@
+cadena = "Aeroshmith"
+
+# Concatenación con símbolo +
+print("Me gusta: " + cadena)
+
+num1 = 3
+num2 = 5
+
+print("operacion suma: " + str(num1 + num2))
+# print("operacion suma: ", num1 + num2)
