a61fc40d1925c7df7be4e3192602b3ea706e1375
Gabriel Perez <gapc87@gmail.com>
2020-05-05 20:52:22 +0200

    funciones argumentos

---
+++ fundamentos/funciones_argumentos.py
@@ -0,0 +1,10 @@
+# parametro es una variable definida entre los parentesis de la funcion
+# argumento (arg) es el valor enviado a la funcion
+
+
+def funcion_arg(nombre, apellido):
+    print("El nombre recibido es:", nombre)
+    print("El apellido recibido es:", apellido)
+
+
+funcion_arg("Gabriel", "Pérez")
