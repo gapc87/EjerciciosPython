4e685adc84baf5d69060469f58c5d6823ca82859
Gabriel Perez <gapc87@gmail.com>
2020-05-05 20:42:16 +0200

    operadores aritméticos

---
+++ fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+a = 3
+b = 2
+suma = a + b
+print('suma', suma)
+
+resta = a - b
+print('resta', resta)
+
+multi = a * b
+print('multiplicacion', multi)
+
+division = a/b
+print('division', division)
+
+modulo = a % b
+print('residuo', modulo)
+
+exponente = a ** b
+print('exponente', exponente)
