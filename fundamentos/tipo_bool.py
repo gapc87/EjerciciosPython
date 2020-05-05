255c528cd5cce42d7156e14e538956712038899c
Gabriel Perez <gapc87@gmail.com>
2020-05-05 20:41:35 +0200

    tipos de datos booleanos

---
+++ fundamentos/tipo_bool.py
@@ -0,0 +1,16 @@
+x = True
+print(x)
+
+y = False
+print(y)
+
+
+num1 = 1
+num2 = 2
+result = num1 > num2
+print(result)
+
+if num1 < num2:
+    print("El valor 1 es menor que el valor 2")
+else:
+    print("El valor 1 no es menor que valor 2")
