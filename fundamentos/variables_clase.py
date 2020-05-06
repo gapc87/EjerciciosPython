9cd3709722bfe0b47be830e06ed4ebd60f410543
Gabriel Perez <gapc87@gmail.com>
2020-05-06 19:16:14 +0200

    variables de clase

---
+++ fundamentos/variables_clase.py
@@ -0,0 +1,32 @@
+class MiClase:
+    variable_clase = "Variable de clase"
+
+    def __init__(self, variable_instancia):
+        self.variable_instancia = variable_instancia
+
+
+print(MiClase.variable_clase)
+objeto1 = MiClase("Variable instancia")
+MiClase.variable_instancia = "Modificando variable de instancia"
+print(MiClase.variable_instancia)
+print(objeto1.variable_instancia)
+
+# Podemos acceder a las variables de clase desde los objetos
+print(objeto1.variable_clase)
+
+# Podemos acceder a la variable con el nombre de la clase
+print(MiClase.variable_clase)
+
+objeto1.variable_clase = "Modificando variable de clase"
+print(objeto1.variable_clase)
+print(MiClase.variable_clase)
+
+objeto2 = MiClase("Nuevo valor de variable de instancia")
+print(objeto2.variable_clase)
+
+objeto3 = MiClase("Tercer objeto")
+
+MiClase.variable_clase = "Cambio desde la clase"
+print(objeto1.variable_clase)
+print(objeto2.variable_clase)
+print(objeto3.variable_clase)
