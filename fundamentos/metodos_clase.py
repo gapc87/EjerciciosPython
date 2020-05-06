2f7aad47c19b5a013bff159acce0c43428663bcd
Gabriel Perez <gapc87@gmail.com>
2020-05-06 19:24:02 +0200

    metodos de clase

---
+++ fundamentos/metodos_clase.py
@@ -0,0 +1,33 @@
+class MiClase:
+
+    variable_clase = "Variable clase"
+
+    def __init__(self):
+        self.variable_instancia = "Variable instancia"
+
+    @staticmethod
+    def metodo_estatico():
+        print("Método estático")
+        print(MiClase.variable_clase)
+        # Desde un método estático no podemos acceder a una variable de instancia
+        # print(MiClase.variable_instancia)
+
+    @classmethod
+    def metodo_clase(cls):
+        print("Método de clase: " + str(cls))
+        print(cls.variable_clase)
+        # No podemos acceder a la variable de instancia desde contexto estático
+        # print(cls.variable_instancia)
+
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+
+
+MiClase.metodo_estatico()
+MiClase.metodo_clase()
+
+print()
+objeto1 = MiClase()
+objeto1.metodo_instancia()
