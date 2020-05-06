777282a577cdf9a04d594aaf4502a13f0ef247e9
Gabriel Perez <gapc87@gmail.com>
2020-05-06 19:17:22 +0200

    constantes

---
+++ fundamentos/test_constantes.py
@@ -0,0 +1,11 @@
+from fundamentos.constantes import MI_CONSTANTE
+from fundamentos.constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+print(Mate.PI)
+
+MI_CONSTANTE = "Nuevo valor"
+print(MI_CONSTANTE)
+
+Mate.PI = 3.14
+print(Mate.PI)
